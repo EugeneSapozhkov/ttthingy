5dc2999793b4046386a5d19fb8b4a788056e7701
liordoe <rakesrunner@gmail.com>
2018-06-01 22:11:50 +0300

    fix2

--- bin/sendInitMessage.js
+++ bin/sendInitMessage.js
@@ -1,10 +1,18 @@
 const axios = require('axios');
 require('dotenv').config();
+function is_weekend(dt){
+    if(dt.getDay() == 6 || dt.getDay() == 0) {
+        return true;
+    }
+    return false;
+}
 
-axios.get(`${process.env.APP_URL}/status`)
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(res => {
-        console.error(res.toString());
-    });
+if (!is_weekend(new Date())) {
+    axios.get(`${process.env.APP_URL}/status`)
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(res => {
+            console.error(res.toString());
+        });
+}
